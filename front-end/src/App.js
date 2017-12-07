d8973b14c3b150810cddfd350fa58a0fddeeb3e1
Saif M <smahmud46@yahoo.com>
2017-12-07 13:13:16 -0500

    more adds

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,18 +1,60 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      students: []
+    }
+  }
+
+
+  componentDidMount(){
+    axios.get('http://localhost:3000/getStudents')
+      .then((response)=>{
+        console.log(response);
+        this.setState({
+          students: response.data
+        })
+      });
+  }
+
+
+  handleSubmit(event){
+    event.preventDefault();
+    const studentName = document.getElementById("new-student").value;
+
+
+    axios({
+      method: "POST",
+      url: "http://localhost:3000/addStudent",
+      data: {
+        studentName: studentName
+      }
+    }).then((data)=>{
+      console.log(data);
+    })
+  }
+
+
   render() {
+    var studentsArray = this.state.students.map((student,i)=>{
+      return(<li key={i}>{student.name} San</li>)
+    })
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Frankie says REACT</h1>
+        
+        <form onSubmit={this.handleSubmit}>
+        <input type="text" id="new-student" placeholder="New Student" />
+        <button type="submit" >Add Student</button>
+        </form>
+        <br />
+        
+        {studentsArray} 
       </div>
     );
   }
